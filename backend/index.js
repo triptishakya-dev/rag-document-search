952b46661e419213d9a51f604f95659a1257a9e0
triptishakya-dev <triptishakya.2001@gmail.com>
2026-02-17 02:08:57 +0530

    feat: Implement document upload functionality with frontend UI, backend processing, and progress tracking.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 const app = express();
 
 const PORT = 3000;
+
+
+app.use(express.json());
+
+
+app.use("/api", uploadRoutes);
 
 app.get("/api/test", (req, res) => {
   return res.json("health is good");
